f62ff1a064a05d12ddd1d89dbc7f77f40ba5ac70
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-03-06 14:35:09 +0800

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,29 @@
+<?php
+$json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
+
+$data = json_decode($json,true);
+$list = $data['records'];
+
+
+?>
+
+<div id="new">
+	<h1>Category list</h1>
+</div>
+
+<table>
+    <tr>
+        <th>Category ID</th>
+        <th>Category Name</th>
+    </tr>
+<?php
+foreach($list as $value){
+?>
+    <tr>
+        <td><?php echo $value['category_id'];?></td>
+        <td><?php echo $value['category_name'];?></td>
+    </tr>
+<?php
+}
+    ?>
+</table>
---
+++ form_delete.php
@@ -0,0 +1,8 @@
+<form action="pro_delete.php" method="POST">
+<div id="head">
+	<h1>Delete Product</h1>
+</div>
+<input type="text" name="id" placeholder="id"/>
+<input type="submit" name="submit" value="submit"/>
+
+</form>
